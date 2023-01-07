5c6c79f3b63d1ed2c546b2bd0f9bc9a1123f83ca
julia111-ops <juliivns@gmail.com>
2023-01-07 18:56:11 -0300

    persona

---
+++ vj/src/main/java/com/vj/vj/VjApplication.java
@@ -0,0 +1,13 @@
+package com.vj.vj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VjApplication.class, args);
+	}
+
+}
---
+++ vj/src/test/java/com/vj/vj/VjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vj.vj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
