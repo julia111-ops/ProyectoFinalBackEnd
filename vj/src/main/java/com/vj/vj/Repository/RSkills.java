fb23b0825b4eb3b20c81d13694add7b56b1e760e
julia111-ops <juliivns@gmail.com>
2023-01-11 15:12:24 -0300

    E/R/S/D/C about/skills/proyectos

---
+++ vj/src/main/java/com/vj/vj/Repository/RSkills.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.vj.vj.Repository;
+
+import com.vj.vj.Entity.Skills;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author lopoj
+ */
+@Repository
+public interface RSkills  extends JpaRepository<Skills, Integer>{
+    public Optional<Skills> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
+}
